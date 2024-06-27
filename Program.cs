7b369e596f6bfeccb0b1f2d8738e1d2de0313ad6
DenSad81 <tagil81@mail.ru>
2024-06-27 18:30:50 +0300

    correct

--- Program.cs
+++ Program.cs
@@ -8,24 +8,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        DrawBar(3, 15, colorOfActivBar: ConsoleColor.Red, posCursorY: 1);
-        DrawBar(8, 15, colorOfActivBar: ConsoleColor.Green, posCursorY: 3);
+        DrawBar(30, colorOfActivBar: ConsoleColor.Red, posCursorY: 1);
+        DrawBar(50, colorOfActivBar: ConsoleColor.Green, posCursorY: 3);
     }
 
-    static void DrawBar(int value, int maxValue, char activBar = '#', char pasivBar = '_', ConsoleColor colorOfActivBar = ConsoleColor.Green, int posCursorX = 0, int posCursorY = 0)
+    static void DrawBar(int value, int maxValue = 10, char activBar = '#', char pasivBar = '_', ConsoleColor colorOfActivBar = ConsoleColor.Green, int posCursorX = 0, int posCursorY = 0)
     {
         Console.SetCursorPosition(posCursorX, posCursorY);
         ConsoleColor memoryOfColor = Console.BackgroundColor;
         Console.BackgroundColor = colorOfActivBar;
         Console.Write('[');
 
-        for (int i = 0; i < value; i++)
-            Console.Write(activBar);
+        float persents = ((float)value / 100.0f);
+        value = (int)(persents * maxValue);
 
-        Console.BackgroundColor = memoryOfColor;
+        if (value > maxValue)
+            value = maxValue;
 
-        for (int i = value; i < maxValue; i++)
-            Console.Write(pasivBar);
+        for (int i = 0; i < maxValue; i++)
+        {
+            if (i < value)
+            {
+                Console.Write(activBar);
+            }
+            else
+            {
+                Console.BackgroundColor = memoryOfColor;
+                Console.Write(pasivBar);
+            }
+        }
 
         Console.Write(']');
         Console.WriteLine();
