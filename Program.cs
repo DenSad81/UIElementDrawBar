bacfe07db3ac22cfcc0c80ef309d15ffea33c734
DenSad81 <tagil81@mail.ru>
2024-06-27 16:03:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DrawBar(3, 15, colorOfActivBar: ConsoleColor.Red, posCursorY: 1);
+        DrawBar(8, 15, colorOfActivBar: ConsoleColor.Green, posCursorY: 3);
+    }
+
+    static void DrawBar(int value, int maxValue, char activBar = '#', char pasivBar = '_', ConsoleColor colorOfActivBar = ConsoleColor.Green, int posCursorX = 0, int posCursorY = 0)
+    {
+        Console.SetCursorPosition(posCursorX, posCursorY);
+        ConsoleColor memoryOfColor = Console.BackgroundColor;
+        Console.BackgroundColor = colorOfActivBar;
+        Console.Write('[');
+
+        for (int i = 0; i < value; i++)
+            Console.Write(activBar);
+
+        Console.BackgroundColor = memoryOfColor;
+
+        for (int i = value; i < maxValue; i++)
+            Console.Write(pasivBar);
+
+        Console.Write(']');
+        Console.WriteLine();
+    }
+}
